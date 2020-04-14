12da8253d3d58271cc80ad43e211f09f46bc7590
Seamus Lee <seamuslee001@gmail.com>
2020-04-14 13:53:36 +1000

    Update to use other thumbnail images

--- CRM/Chmosaicotemplate/TemplateWrapper.php
+++ CRM/Chmosaicotemplate/TemplateWrapper.php
@@ -9,12 +9,24 @@ class CRM_Chmosaicotemplate_TemplateWrapper implements API_Wrapper {
   public function toApiOutput($apiRequest, $result) {
     foreach ($result['values'] as $key => $res) {
       if (isset($res['metadata'])) {
+        $metadata = json_decode($res['metadata'], TRUE);
         if ($res['title'] == 'Basic - Email With Gallery') {
-          $metadata = json_decode($res['metadata'], TRUE);
           $metadata['thumbnail'] = '/vendor/civicrm/canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Email%20With%20Gallery.jpg';
-          $result['values'][$key]['metadata'] = json_encode($metadata);
+        }
+        elseif ($res['title'] == 'Basic - Email No Gallery') {
+          $metadata['thumbnail'] = '/vendor/civicrm/canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Email%20No%20Gallery.jpg';
+        }
+        elseif ($res['title'] == 'Basic - Newsletter') {
+          $metadata['thumbnail'] = '/vendor/civicrm/canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Newsletter.jpg';
+        }
+        elseif ($res['title'] == 'Basic - Text Only') {
+          $metadata['thumbnail'] = '/vendor/civicrm/canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Text%20Only.jpg';
+        }
+        elseif ($res['title'] == 'Basic - Thank You Email') {
+          $metadata['thumbnail'] = '/vendor/civicrm/canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Thank%20You%20Email.jpg';
         }
       }
+      $result['values'][$key]['metadata'] = json_encode($metadata);
     }
     return $result;
   }
